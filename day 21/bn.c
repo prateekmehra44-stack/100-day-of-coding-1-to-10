1996b14f407b901228a7385c848d353b83c4bfc6
prateekmehra44-stack <prateekmehra44@gmail.com>
2025-10-15 09:18:53 +0530

    Add files via upload

---
+++ day 21/bn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char binary[100];
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    for (int i = 0; i < strlen(binary); i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+        else {
+            printf("Invalid binary number.\n");
+            return 0;
+        }
+    }
+
+    printf("1's complement = %s\n", binary);
+    return 0;
+}
---
+++ day 21/od product.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, product = 1, hasOdd = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+        if (digit % 2 != 0) {
+            product *= digit;
+            hasOdd = 1;
+        }
+        n /= 10;
+    }
+
+    if (hasOdd)
+        printf("Product of odd digits = %d\n", product);
+    else
+        printf("No odd digits found.\n");
+
+    return 0;
+}

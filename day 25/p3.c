d8b57f4fe93251586acc42c89b95dd3c91024637
prateekmehra44-stack <prateekmehra44@gmail.com>
2025-10-15 09:20:37 +0530

    Add files via upload

---
+++ day 25/p3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, spaces;
+    for(i = 5; i >= 1; i--) {
+        for(spaces = 5 - i; spaces > 0; spaces--) {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

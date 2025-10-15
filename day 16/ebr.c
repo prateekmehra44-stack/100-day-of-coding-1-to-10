24c92283e46e890da047c9f966019dfe604b2020
prateekmehra44-stack <prateekmehra44@gmail.com>
2025-10-15 09:16:49 +0530

    Add files via upload

---
+++ day 16/ebr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, binary[32], i = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("Binary: 0\n");
+        return 0;
+    }
+
+    while (n > 0) {
+        binary[i] = n % 2;
+        n /= 2;
+        i++;
+    }
+
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+    
+    return 0;
+}
